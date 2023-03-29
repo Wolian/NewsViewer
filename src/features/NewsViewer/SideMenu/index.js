be636c3aa515bff41dd9ef355bff5ba4a533ce4b
Wolian <krzys_banasiak@o2.pl>
2023-03-29 16:15:50 +0200

    side menu added

---
+++ src/features/NewsViewer/SideMenu/index.js
@@ -0,0 +1,11 @@
+import { CountryList, Item, Wrapper } from "./styled";
+
+export const SideMenu = () => (
+    <Wrapper>
+        <CountryList>
+            <Item>Polska</Item>
+            <Item>Niemcy</Item>
+            <Item>Czechy</Item>
+        </CountryList>
+    </Wrapper>
+);
---
+++ src/features/NewsViewer/SideMenu/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+    display: flex;
+    
+`;
+
+export const CountryList = styled.ul`
+
+`;
+
+export const Item = styled.li`
+    
+`;
