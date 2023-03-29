a6d4168b5dd4af380d1d4f1d01f8e9750192e5d0
Wolian <krzys_banasiak@o2.pl>
2023-03-29 16:14:42 +0200

    app basic layout added

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,9 +1,17 @@
+import { Footer } from "../../features/NewsViewer/Footer";
+import { Header } from "../../features/NewsViewer/Header";
+import { MainContent } from "../../features/NewsViewer/MainContent";
+import { SideMenu } from "../../features/NewsViewer/SideMenu";
+
 export const App = () => {
   return (
     <>
-      <div>
-        Dzień dobry!
-      </div>
+      <Header />
+      <main>
+        <SideMenu />
+        <MainContent />
+      </main>
+      <Footer />
     </>
   );
 };
