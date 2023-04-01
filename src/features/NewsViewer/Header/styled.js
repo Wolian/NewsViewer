85a13e7b870f02ac306aa0257df26b9da7b01667
Wolian <krzys_banasiak@o2.pl>
2023-04-01 19:37:36 +0200

    header styling added

--- src/features/NewsViewer/Header/styled.js
+++ src/features/NewsViewer/Header/styled.js
@@ -1,21 +1,42 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.header`
-    display: flex;
-    justify-content: space-around;
-    padding: 10px;
-    background-color: ${({ theme }) => theme.colors.Gallery};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 60px;
+  background-color: ${({ theme }) => theme.colors.BrightTurquoise};
+  background-image: linear-gradient(
+    315deg,
+    ${({ theme }) => theme.colors.BrightTurquoise} 0%,
+    ${({ theme }) => theme.colors.ElectricViolet} 75%
+  );
 `;
 
-export const GnNews = styled.div`
-    font-weight: 600;
-    font-size: 40px;
+export const NewsViewer = styled.h1`
+  font-size: 40px;
+  margin-bottom: 20px;
+  line-height: 55px;
+  color: ${({ theme }) => theme.colors.White};
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `;
 
 export const ViewButton = styled.button`
-
+  margin-bottom: 20px;
+  border-radius: 8px;
+  padding: 10px;
+  
 `;
 
 export const PopupButton = styled.button`
-
+  margin-bottom: 20px;
+  border-radius: 8px;
+  padding: 10px;
 `;
