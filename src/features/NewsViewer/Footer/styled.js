5e4806a3745b28cb4fa51c87456375554c212936
Wolian <krzys_banasiak@o2.pl>
2023-04-01 19:38:11 +0200

    footer styles added

--- src/features/NewsViewer/Footer/styled.js
+++ src/features/NewsViewer/Footer/styled.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export const Wrapper = styled.footer`
   display: flex;
   justify-content: space-between;
-  font-weight: 700;
-  font-size: 22px;
-  padding: 10px 20px;
-  background-color: ${({ theme }) => theme.colors.Gallery};
+  padding: 20px 60px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.BrightTurquoise};
+  background-image: linear-gradient(315deg, ${({ theme }) => theme.colors.BrightTurquoise} 0%, ${({ theme }) => theme.colors.ElectricViolet} 85%);
 `;
 
-export const Time = styled.div`
-    border-right: 1px solid ${({ theme }) => theme.colors.Gallery};
-    padding: 0 20px;
+export const Text = styled.div`
+  padding: 0 20px;
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.White};
 `;
