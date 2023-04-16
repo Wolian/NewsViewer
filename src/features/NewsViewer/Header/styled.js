8ccff573b7ff9b504543943b2e955f5a2ff4d8d3
Wolian <krzys_banasiak@o2.pl>
2023-04-16 14:47:40 +0200

    boxshadow added to button

--- src/features/NewsViewer/Header/styled.js
+++ src/features/NewsViewer/Header/styled.js
@@ -32,7 +32,7 @@ export const ViewButton = styled.button`
   margin-bottom: 20px;
   border-radius: 8px;
   padding: 10px;
-  
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.14), 0px 2px 6px rgba(0, 0, 0, 0.14), 0px 0px 1px rgba(0, 0, 0, 0.14);
 `;
 
 export const PopupButton = styled.button`
