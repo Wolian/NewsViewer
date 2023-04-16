4176fa18dbdb1a2b9607ba228c828e6d188ab3d3
Wolian <krzys_banasiak@o2.pl>
2023-04-16 14:47:08 +0200

    background color changed

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
     
     body {
         font-family: 'Inter', sans-serif;
-        
+        background-color: ${({ theme }) => theme.colors.whiteLilac};
     }
 
 `;
