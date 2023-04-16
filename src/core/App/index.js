54e7b0f29bd2c5478dc3a869c87a942d96a9aed8
Wolian <krzys_banasiak@o2.pl>
2023-04-16 14:46:38 +0200

    app structure reorganized

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,17 +1,21 @@
+import { ThemeProvider } from "styled-components";
 import { Footer } from "../../features/NewsViewer/Footer";
 import { Header } from "../../features/NewsViewer/Header";
 import { MainContent } from "../../features/NewsViewer/MainContent";
 import { SideMenu } from "../../features/NewsViewer/SideMenu";
+import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme";
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Header />
       <main>
         <SideMenu />
         <MainContent />
       </main>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import { App } from "./core/App";
-import { GlobalStyle } from "./core/App/GlobalStyle";
 import reportWebVitals from "./reportWebVitals";
-import { theme } from "./core/App/theme";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
