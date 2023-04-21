47768e29348b076cb8179cdbd95cc51800af7b59
Wolian <krzys_banasiak@o2.pl>
2023-04-21 14:03:48 +0200

    color small fixes

--- src/features/NewsViewer/SideMenu/styled.js
+++ src/features/NewsViewer/SideMenu/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.aside`
     display: flex;
-    background-color: ${({ theme }) => theme.colors.White};;
+    background-color: ${({ theme }) => theme.colors.white};;
     width: 25%;
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
     margin: 20px;
