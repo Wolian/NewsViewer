47c3cf8d0e4a7ac30695cea1a047bfceb4dd942b
Wolian <krzys_banasiak@o2.pl>
2023-04-21 14:04:15 +0200

    News display from API

--- src/features/NewsViewer/MainContent/index.js
+++ src/features/NewsViewer/MainContent/index.js
@@ -1,10 +1,34 @@
-import { News, Wrapper } from "./styled";
+import { useEffect, useState } from "react";
+import { Item, Link, News, Wrapper } from "./styled";
+import axios from "axios";
+import { APIKey } from "../../../core/API";
 
-export const MainContent = () => (
+export const MainContent = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://newsapi.org/v2/top-headlines?country=pl&apiKey=${APIKey}`)
+      .then((response) => {
+        setNews(response.data.articles);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
     <Wrapper>
-        <News>News1</News>
-        <News>News2</News>
-        <News>News3</News>
-        <News>News4</News>
+      <News>
+        {news.map(({ id, url, title, content }) => (
+          <Item key={id}>
+            <Link href={url} target="_blank" rel="noreferrer">
+              <h2>{title}</h2>
+              <p>{content}</p>
+            </Link>
+          </Item>
+        ))}
+      </News>
     </Wrapper>
-);
+  );
+};
