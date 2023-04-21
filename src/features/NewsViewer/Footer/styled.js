2181d350be84e56803563e8f2737df591866eb6c
Wolian <krzys_banasiak@o2.pl>
2023-04-21 14:03:27 +0200

    colors small fixes

--- src/features/NewsViewer/Footer/styled.js
+++ src/features/NewsViewer/Footer/styled.js
@@ -5,12 +5,12 @@ export const Wrapper = styled.footer`
   justify-content: space-between;
   padding: 20px 60px;
   width: 100%;
-  background-color: ${({ theme }) => theme.colors.BrightTurquoise};
-  background-image: linear-gradient(315deg, ${({ theme }) => theme.colors.BrightTurquoise} 0%, ${({ theme }) => theme.colors.ElectricViolet} 85%);
+  background-color: ${({ theme }) => theme.colors.brightTurquoise};
+  background-image: linear-gradient(315deg, ${({ theme }) => theme.colors.brightTurquoise} 0%, ${({ theme }) => theme.colors.electricViolet} 85%);
 `;
 
 export const Text = styled.div`
   padding: 0 20px;
   font-size: 20px;
-  color: ${({ theme }) => theme.colors.White};
+  color: ${({ theme }) => theme.colors.white};
 `;
