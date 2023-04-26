b5f9a3b5f811255542d5b65c0e1363a1ce6b4c6a
Wolian <krzys_banasiak@o2.pl>
2023-04-26 15:09:42 +0200

    API moved into another component

--- src/features/NewsViewer/API.js
+++ src/features/NewsViewer/API.js
@@ -1,1 +1,15 @@
+import axios from "axios";
+
 export const APIKey = "dbdd5a6286424577a15e9bf27f770b1e";
+
+export const NewsViewerAPI = () => {
+    return axios
+    .get(`https://newsapi.org/v2/top-headlines?country=pl&apiKey=${APIKey}`)
+    .then((response) => {
+        return response.data.articles;
+    })
+    .catch((error) => {
+        console.log(error);
+        return [];
+    });
+};
